97da9ccdc90b88075e7d6f63bd57ca6c9b91620a
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-12 02:50:12 +0300

    err

--- models/card.js
+++ models/card.js
@@ -4,42 +4,42 @@ const user = require('./user');
 
 const cardSchema = new mongoose.Schema(
   {
-  name: {
-    type: String,
-    minLength: [2, 'Минимальная длина поля "name" - 2 знака'],
-    maxLength: [30, 'Максимальная длина поля "name" - 30 знаков'],
-    required: [true, 'Необходимо заполнить поле "name"'],
-  },
-  link: {
-    type: String,
-    required: [true, 'Необходимо заполнить поле "link"'],
-    validate: {
-      validator: (url) => {
-        validator.isURL(url);
-      },
+    name: {
+      type: String,
+      minLength: [2, 'Минимальная длина поля "name" - 2 знака'],
+      maxLength: [30, 'Максимальная длина поля "name" - 30 знаков'],
+      required: [true, 'Необходимо заполнить поле "name"'],
+    },
+    link: {
+      type: String,
+      required: [true, 'Необходимо заполнить поле "link"'],
+      validate: {
+        validator: (url) => {
+          validator.isURL(url);
+        },
       message: 'Введён неккоректный URL',
+      },
+    },
+    owner: {
+      ref: user,
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: {
+      type: [
+        {
+          ref: 'user',
+          type: mongoose.Schema.Types.ObjectId,
+        },
+      ],
+      default: [],
+      required: true,
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  owner: {
-    ref: user,
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  likes: {
-    type: [
-      {
-        ref: 'user',
-        type: mongoose.Schema.Types.ObjectId,
-      },
-    ],
-    default: [],
-    required: true,
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-},
 {
   versionKey: false,
 });
