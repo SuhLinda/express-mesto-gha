e8398a8c457e36db010e108f57d4deb854f8f0fa
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-21 18:03:28 +0300

    hotfix/v4

--- middlewares/validationData.js
+++ middlewares/validationData.js
@@ -1,68 +1,54 @@
 const { celebrate, Joi } = require('celebrate');
 const { regExp } = require('../utils/constants');
 
-function validationUserId() {
-  celebrate({
-    params: Joi.object().keys({
-      id: Joi.string().alphanum().hex().length(24),
-    }),
-  });
-}
+const validationUserId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().hex().length(24),
+  }),
+});
 
-function validationCreateUser() {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri().regex(regExp),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  });
-}
+const validationCreateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri().regex(regExp),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
 
-function validationLogin() {
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  });
-}
+const validationLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
 
-function validationUpdateProfile() {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  });
-}
+const validationUpdateProfile = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+});
 
-function validationUpdateAvatar() {
-  celebrate({
-    body: Joi.object().keys({
-      avatar: Joi.string().uri().regex(regExp),
-    }),
-  });
-}
+const validationUpdateAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri().regex(regExp),
+  }),
+});
 
-function validationCreateCard() {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri().regex(regExp),
-    }),
-  });
-}
+const validationCreateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri().regex(regExp),
+  }),
+});
 
-function validationCardId() {
-  celebrate({
-    params: Joi.object().keys({
-      id: Joi.string().alphanum().hex().length(24),
-    }),
-  });
-}
+const validationCardId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().hex().length(24),
+  }),
+});
 
 module.exports = {
   validationUserId,
