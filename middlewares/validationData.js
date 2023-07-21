b425d54fc3c0cf09c8678b79e5a37db254d2a47f
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-21 20:12:56 +0300

    hotfix/v5

--- middlewares/validationData.js
+++ middlewares/validationData.js
@@ -12,7 +12,7 @@ const validationCreateUser = celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().uri().regex(regExp),
-    email: Joi.string().required().email()ы
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 });
