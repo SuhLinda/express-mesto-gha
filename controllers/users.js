5cfe3642f643daee424d21f99a07e863a12d15b3
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-22 17:28:53 +0300

    b

--- controllers/users.js
+++ controllers/users.js
@@ -63,9 +63,9 @@ function createUser(req, res, next) {
       if (err.name === 'ValidationError') {
         next(new ErrorBadRequest('Переданы некорректные данные'));
       } else if (err.code === 11000) {
-        return next(new ErrorUserExists('Пользователь с таким email существует'));
+        next(new ErrorUserExists('Пользователь с таким email существует'));
       } else {
-        return next(err);
+        next(err);
       }
     });
 }
