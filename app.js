6f221e5456fc3323ec01890f1d5d00f398789ee2
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-23 20:52:25 +0300

    hotfix/v3

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const limiter = require('./utils/constants');
 const auth = require('./middlewares/auth');
+const limiter = require('./middlewares/rateLimit');
+
 const handleError = require('./middlewares/handleError');
 const entrance = require('./routes/auth');
 const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
 const routesNotFound = require('./routes/errorNotFound');
 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 const app = express();
 mongoose.connect(DB_URL);
 
 app.use(helmet());
-app.use(limiter);
-
 app.use(bodyParser.json());
+app.use(limiter);
 
 app.use(entrance);
 app.use(auth, routesUsers);
