215dcac77842db2a47697e1566cd063902501588
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-23 16:54:36 +0300

    hotfix/v3

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,9 @@ const {
   updateAvatar,
 } = require('../controllers/users');
 
-router.get('/users/me', validationUserId, getUserMe);
+router.get('/users/me', getUserMe);
 router.get('/users', getUsers);
-router.get('/users/:id', getUser);
+router.get('/users/:id', validationUserId, getUser);
 router.patch('/users/me', validationUpdateProfile, updateProfile);
 router.patch('/users/me/avatar', validationUpdateAvatar, updateAvatar);
 
