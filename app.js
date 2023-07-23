8766c26de2f890165bd15b1e448b6279d53a4360
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-07-23 14:07:29 +0300

    hotfix/v2

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
+const { PORT = 3000 } = process.env;
 const bodyParser = require('body-parser');
-
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const handleError = require('./middlewares/handleError');
-
 const entrance = require('./routes/auth');
 const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
-
-const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -25,10 +22,6 @@ app.use(auth, routesCards);
 app.use(errors());
 app.use(handleError);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT);
